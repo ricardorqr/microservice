ddcc372e0b1031f26cd2af6fc2d2c44d30559ad3
Ricardo Ribeiro <ricardo@lyricfind.com>
2022-01-23 13:06:46 -0500

    Install multi modules

---
+++ service-user/src/main/java/com/serviceUser/entity/User.java
@@ -0,0 +1,13 @@
+package com.serviceUser.entity;
+
+import com.serviceDepartment.entity.Department;
+
+public class User {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Department department;
+
+}
